c2f8f883a25a75c6222765683ab0db55736af981
Salman Titas <salman.mehedy@gmail.com>
2019-03-25 01:35:16 -0700

    lift added

---
+++ Lift/src/com/euhedral/game/MouseInput.java
@@ -0,0 +1,60 @@
+package com.euhedral.game;
+
+import com.euhedral.engine.EngineMouse;
+import com.euhedral.game.GameController;
+
+import java.awt.event.MouseAdapter;
+
+public class MouseInput extends MouseAdapter {
+    private GameController gameController;
+    private EngineMouse mouse;
+
+    public MouseInput(EngineMouse engineMouse, GameController gameController) {
+        this.gameController = gameController;
+        mouse = engineMouse;
+    }
+
+    public void updatePressed() {
+
+    }
+
+    public void updateReleased() {
+
+    }
+
+    public void updateMoved() {
+
+    }
+
+    public int getMx() {
+        return mouse.getMx();
+    }
+
+    public int getMy() {
+        return mouse.getMy();
+    }
+
+    public int getMxPressed() {
+        return mouse.getMxPressed();
+    }
+
+    public int getMyPressed() {
+        return mouse.getMyPressed();
+    }
+
+    public int getMxReleased() {
+        return mouse.getMxReleased();
+    }
+
+    public int getMyReleased() {
+        return mouse.getMyReleased();
+    }
+
+    public void checkButtonPressed() {
+        gameController.checkButtonAction(getMxPressed(), getMyPressed());
+    }
+
+    public void checkButtonReleased() {
+        gameController.checkButtonAction(getMxReleased(), getMyReleased());
+    }
+}
